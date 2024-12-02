4cd0307ff3dff4fc6e638e072b5ade692fa342e6
XxCODSMACKxX <naumanhunter@yahoo.com>
2024-12-02 11:10:02 -0600

    Updated day 1 code to include second part

--- day_1/src.py
+++ day_1/src.py
@@ -1,5 +1,7 @@
 import os
 
+# Part 1
+
 # Identify path to puzzle input and read in data
 src_dir = os.path.dirname(os.path.realpath(__file__))
 input_data = open(os.path.join(src_dir, 'puzzle_input.txt'), 'r').readlines()
@@ -24,5 +26,22 @@ for index, _ in enumerate(list_0):
 
 # Calculate total distance
 total_distance = sum(differences)
+print(f'Result for part 1: {total_distance}')
 
-print(total_distance)
+
+# Part Two
+list_1_counts = {}
+
+# Create a count of each element in the second list
+for element in list_1:
+    if element not in list_1_counts:
+        list_1_counts[element] = 1
+    else:
+        list_1_counts[element] += 1
+
+# Calculate the element similarity scores between the two lists by multiplying the element in the first list by the count of the second list
+element_similarity_scores = [element * list_1_counts[element] if element in list_1_counts else 0 for element in list_0]
+
+# Calculate list similarity score by summing all element similarity scores
+list_similarity_score = sum(element_similarity_scores)
+print(f'Result for part 2: {list_similarity_score}')
